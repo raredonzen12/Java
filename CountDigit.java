57090fc04a830429ae06b25ec8ada9412552dc46
raredonzen12 <84237721+raredonzen12@users.noreply.github.com>
2022-12-04 22:54:00 +0530

    java program to find the digit in the given number

---
+++ CountDigit.java
@@ -0,0 +1,21 @@
+package java;
+
+import java.util.Scanner;
+
+class CountDigit{
+    static int count(int n){
+        int count =0;
+        while(n>0){
+            n/=10;
+            count++;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(count(n));
+        sc.close();
+    }
+}
