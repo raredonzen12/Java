da5a8610ea92dc4efb59b6d52ebc12b286e255d8
raredonzen12 <84237721+raredonzen12@users.noreply.github.com>
2022-12-05 19:58:04 +0530

    find GCD/HCF  using modified euclid method

--- GCDOrHFC.java
+++ GCDOrHFC.java
@@ -2,14 +2,17 @@ import java.util.Scanner;
 
 public class GCDOrHFC {
     static int euclidGcd(int a, int b){
-        while(a!=b){
+        while(a != 0 && b != 0 ){
             if(a>b){
-                a = a-b;
+                a = a%b;
             }else{
-                b = b-a;
+                b = b%a;
             }
         }
+        if(a != 0)
         return a;
+        else
+        return b;
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
