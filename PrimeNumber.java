c829f69e6dd1cb3856bd9e65ce6e0582ccfac007
raredonzen12 <84237721+raredonzen12@users.noreply.github.com>
2022-12-06 22:54:16 +0530

    Check prime or not

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -2,8 +2,20 @@ import java.util.Scanner;
 
 public class PrimeNumber {
     static boolean isPrime(int n){
-        for(int i=2; i<=Math.sqrt(n); i++){
-            if(n%i == 0){
+        if(n==1){
+            return false;
+        }
+
+        else if(n==2 || n==3){
+            return true;
+        }
+        
+        else if(n%2==0 || n%3==0){
+            return false;
+        }
+        
+        for(int i=2; i<=Math.sqrt(n); i=i+6){
+            if(n%i == 0 || n%(i+2)==0){
                 return false;
             }
         }
