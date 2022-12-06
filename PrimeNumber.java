853c2868767e4b8187003b7a12486d6be5577b0a
raredonzen12 <84237721+raredonzen12@users.noreply.github.com>
2022-12-06 22:56:15 +0530

    Check prime or not

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -14,7 +14,7 @@ public class PrimeNumber {
             return false;
         }
         
-        for(int i=2; i<=Math.sqrt(n); i=i+6){
+        for(int i=5; i<=Math.sqrt(n); i=i+6){
             if(n%i == 0 || n%(i+2)==0){
                 return false;
             }
