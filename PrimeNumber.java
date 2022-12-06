800e12a121c5f56a5e07bfd67919131f45f008ee
raredonzen12 <84237721+raredonzen12@users.noreply.github.com>
2022-12-06 20:53:33 +0530

    Check prime number or not 2nd type

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class PrimeNumber {
     static boolean isPrime(int n){
-        for(int i=2; i<=n/2; i++){
+        for(int i=2; i<=Math.sqrt(n); i++){
             if(n%i == 0){
                 return false;
             }
